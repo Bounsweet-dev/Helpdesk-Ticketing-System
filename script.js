71a21c6afbaec5b2e5f2789b728a02b14072909f
Bounsweet-dev <djeriemmy@gmail.com>
2026-09-10 00:39:54 +0800

    Completed ticket list UI design with HTML, CSS, and JS

--- script.js
+++ script.js
@@ -1,50 +1,8 @@
-const dashboardButton = document.getElementById("dashboardButton");
-const ticketButton = document.getElementById("ticketButton");
-const createTicketButton = document.getElementById("createTicketButton");
-const knowledgeBaseButton = document.getElementById("knowledgeBaseButton");
+const filterButton = document.getElementById("main-ticket-list-filter-button");
+const ticketFilters = document.querySelector(".main-ticket-list-filter");
 
-const content = document.getElementById("content");
+filterButton.addEventListener("click", function() {
 
-ticketButton.addEventListener("click", function(){
+    ticketFilters.classList.toggle("collapsed");
 
-    content.innerHTML = `
-        <h1>Ticket List</h1>
-
-        <ul>
-            <li>Ticket 1</li>
-            <li>Ticket 2</li>
-            <li>Ticket 3</li>
-        </ul>
-    `;
-});
-
-dashboardButton.addEventListener("click", function(){
-
-    content.innerHTML = `
-        <h1>Dashboard</h1>
-
-        <p>Welcome to the Dashboard!</p>
-    `;
-});
-
-createTicketButton.addEventListener("click", function() {
-    
-    content.innerHTML = `
-        <h1>Create Ticket</h1>
-
-        <p>"Ticket type and stuff to add"</p>
-    `;
-});
-
-knowledgeBaseButton.addEventListener("click", function() {
-    
-    content.innerHTML = `
-        <h1>Knowledge Bases</h1>
-
-        <ul>
-            <li>Knowledge Base 1</li>
-            <li>Knowledge Base 2</li>
-            <li>Knowledge Base 2</li>
-        </ul>
-    `;
 });
